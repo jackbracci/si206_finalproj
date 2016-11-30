137d40dbde1d66d34f78bc7ae2e44437d9fbb0e8
Jonathan Bracci <jackb1412@gmail.com>
2016-11-30 13:40:39 -0500

    Working paddle

--- bo1.py
+++ bo1.py
@@ -5,8 +5,15 @@ pygame.init();
 white = (255,255,255)
 black = (0,0,0)
 red = (255,0,0)
-blue = (0,0,255)
-green = (125,175,125)
+blue = (70,130,180)
+green = (127,255,212)
+Width = 800
+Height = 600
+x_pos = 350
+y_pos = 580
+x = 400
+y = 300
+
 
 
  
@@ -14,46 +21,41 @@ class Paddle(pygame.sprite.Sprite):
 
 
 	def __init__(self):
-		pygame.sprite.Sprite. __init__(self)
+		pygame.sprite.Sprite. __init__ (self)
 		self.width = 100
 		self.height = 20
 		self.image = pygame.Surface([self.width,self.height])
 		self.image.fill(blue)
 
 		self.rect = self.image.get_rect()
-		self.rect = self.image.get_rect()
-		self.screenheight = pygame.display.get_surface().get_height()
-		self.screenwidth = pygame.display.get_surface().get_width()
-		self.rect.x = 0
-		self.rect.y = self.screenheight-self.height
+		self.rect.x = x_pos
+		self.rect.y = y_pos
 
-	def update(self, action):
-		pos = pygame.mouse.get_pos()
-		self.rect.x = pos[0]
-		if self.rect.x > self.screenwidth - self.width:
-			self.rect.x = self.screenwidth - self.width
+	def update(self):
+		self.rect.x = x_pos
+		screen.fill(white)
 	
 
-# class Ball(pygame.sprite.Sprite):
-# 	degree = 180 
-# 	speed = 10
+class Ball(pygame.sprite.Sprite):
+	degree = 180 
+	speed = 10
 
-# 	def __init__(self):
-# 		super().__init__()
-# 		self.width = 15
-# 		self.height = 15
-# 		self.image = pygame.Surface([self.width,self.height])
-# 		self.image.fill(blue)
+	def __init__(self):
+		pygame.sprite.Sprite. __init__ (self)
+		self.width = 15
+		self.height = 15
+		self.image = pygame.Surface([self.width,self.height])
+		self.image.fill(blue)
 
-# 		self.rect = self.image.get_rect()
-# 		self.rect.x = x
-# 		self.rect.y = y
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
 
 
 class Block(pygame.sprite.Sprite):
  
     def __init__(self, color, x, y):
-        super().__init__()
+        pygame.sprite.Sprite. __init__ (self)
 
         self.image = pygame.Surface([block_width, block_height])
         self.image.fill(color)
@@ -74,36 +76,34 @@ clock = pygame.time.Clock()
 
 blocks = pygame.sprite.Group()
 sprites = pygame.sprite.Group()
-# ball = Ball()
-# sprites.add(ball)
+ball = Ball()
+sprites.add(ball)
 paddle = Paddle()
 sprites.add(paddle)
-# --- Create blocks
  
-# Five rows of blocks
+
 for row in range(5):
-    # 32 columns of blocks
     for column in range(0, blockcount):
-        # Create a block (color,x,y)
         block = Block(green, column * (block_width + 2) , top)
         blocks.add(block)
         sprites.add(block)
-    # Move the top of the next row down
     top += block_height + 2
-gameDisplay = pygame.display.set_mode((800, 600))
+gameDisplay = pygame.display.set_mode((Width, Height))
 screen = gameDisplay
-screen.fill(black)
+
 
 pygame.display.set_caption("Breakout Project")
 
-
+screen.fill(white)
 
 gameExit = False
 
 while not gameExit:
 	# Ball()
-	Paddle()
+	# Paddle()
 	# Block()
+	
+	#clock.tick(0)
 	
 	
 	pygame.display.update()
