a649644ed7732faaa0bf360e9bf2562c62afa49b
Jonathan Bracci <jackb1412@gmail.com>
2016-12-01 03:56:38 -0500

    firing and score

--- bo1.py
+++ bo1.py
@@ -15,8 +15,10 @@ blue = (70,130,180)
 green = (127,255,212)
 Width = 800
 Height = 600
+speed = 5 
 x_pos = 350
 y_pos = 580
+
 # x = 400
 # y = 300
 # a = 400
@@ -29,8 +31,8 @@ class Paddle(pygame.sprite.Sprite):
 
 	def __init__(self):
 		pygame.sprite.Sprite. __init__ (self)
-		self.width = 100
-		self.height = 20
+		self.width = 40
+		self.height = 40
 		self.image = pygame.Surface([self.width,self.height])
 		self.image.fill(blue)
 
@@ -40,7 +42,7 @@ class Paddle(pygame.sprite.Sprite):
 
 	def update(self):
 		self.rect.x = x_pos
-		screen.fill(white)
+		screen.fill(black)
 	
 
 class Block(pygame.sprite.Sprite):
@@ -53,20 +55,60 @@ class Block(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+class Bullet(pygame.sprite.Sprite):
+
+    def __init__(self):
+
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.Surface([4, 10])
+        self.image.fill(red)
+
+        self.rect = self.image.get_rect()
+
+    def update(self):
+
+
+
+        self.rect.y -= 10
+
 class Falling(pygame.sprite.Sprite):
 	def __init__(self):
 		pygame.sprite.Sprite.__init__(self)
 		self.image = pygame.Surface((30,40))
 		self.image.fill(red)
 		self.rect = self.image.get_rect()
+		self.rect.x = random.randrange(Width - self.rect.width)
 	def back_to_top(self):
-		self.rect.y = random.randrange(-300, -20)
-		self.rect.x = random.range(0, Height)
+		self.rect.y = random.randrange(-400, -50)
+		self.rect.x = random.randrange(0, Height)
 
 	def update(self):
-		self.rect.y += 3
+		self.rect.y += 15
 		if self.rect.y>610:
 			self.back_to_top()
+
+class Score(pygame.sprite.Sprite):
+	def __init__(self):
+		pygame.sprite.Sprite.__init__(self)
+		self.score = 0
+		self.font = pygame.font.SysFont('monospace', 20)
+
+	def draw(self, screen):
+		txt = self.font.render("SCORE:" + str(self.score) + " Lives 1", True, white)
+		screen.blit(txt, (0, 0))
+
+	def add(self):
+		self.score += 1
+
+	def val(self):
+		return self.score
+
+	def gameover(self, screen):
+		txt = self.font.render('GAMEOVER! You Scored ' + str(self.score)+ ' points!', True , white)
+		screen.blit(txt, (400 - txt.get_width() / 2, 100))
+
+
 # class Mob(pygame.sprite.Sprite):
 #     # mob sprite - spawns above top and moves downward
 #     def __init__(self):
@@ -88,21 +130,22 @@ class Falling(pygame.sprite.Sprite):
 init()
 top = 50
 blockcount = 32
-block_width = 100
+block_width = 50
 block_height = 20
-
+f = font.Font(None, 25)
 clock = pygame.time.Clock()
 
-
+bullet = pygame.sprite.Group()
 blocks = pygame.sprite.Group()
 sprites = pygame.sprite.Group()
 # ball = Ball()
 # sprites.add(ball)
+score = Score()
 paddle = Paddle()
 sprites.add(paddle)
- 
+bullet_list = pygame.sprite.Group()
 falling = pygame.sprite.Group()
-falling = Falling()
+# falling = Falling()
 sprites.add(falling)
 
 for row in range(5):
@@ -114,6 +157,10 @@ for row in range(5):
 gameDisplay = pygame.display.set_mode((Width, Height))
 screen = gameDisplay
 
+for i in range(10):
+	fall = Falling()
+	sprites.add(fall)
+	falling.add(fall)
 
 pygame.display.set_caption("Jack's Project")
 
@@ -122,6 +169,7 @@ screen.fill(white)
 
 
 gameExit = False
+game_over = False
 mixer.music.load("closer.wav")
 mixer.music.play(-1)
 
@@ -137,16 +185,46 @@ while not gameExit:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			gameExit = True
-	
+		elif event.type == pygame.MOUSEBUTTONDOWN:
+			bullet = Bullet()
+			bullet.rect.x = paddle.rect.x
+			bullet.rect.y = paddle.rect.y
+			sprites.add(bullet)
+			bullet_list.add(bullet)
+
 	if event.type == pygame.KEYDOWN:
 		if event.key == pygame.K_LEFT:
 			x_pos -= 10
 		if event.key == pygame.K_RIGHT:
 			x_pos += 10
 
+	for bullet in bullet_list:
+		block_hit_list = pygame.sprite.spritecollide(bullet, blocks, True)
+
+		for block in block_hit_list:
+			bullet_list.remove(bullet)
+			sprites.remove(bullet)
+			score.add()
+			
+			# score += 1 
+			# print (score)
+		if bullet.rect.y < -10:
+			bullet_list.remove(bullet)
+			sprites.remove(bullet)
+
+	if pygame.sprite.spritecollide(bullet,sprites):
+		game_over = True
+	if game_over == True:
+		score.gameover(screen)
+
+
+
+	t = f.render("Score = " + str(score), False, (0,0,0))
+	screen.blit(t, (320, 0))
 
 	sprites.update()
 	sprites.draw(screen)
+	score.draw(screen)
 	pygame.display.update()		
 
 
